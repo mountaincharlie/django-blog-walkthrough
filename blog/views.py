9a251c2366d7fa1cd3a2178cc0550177a27d0745
Charlie <ciharland@hotmail.co.uk>
2022-04-07 09:02:17 +0000

    Create PostList views class

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    # getting all the 'Published' status posts in date order
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    # the template our view will render
+    template_name = 'index.html'
+    # number of posts per page
+    paginate_by = 6

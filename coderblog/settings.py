18a82c36a8af66c701c769a81fe8b4c728f7b8fa
Charlie <ciharland@hotmail.co.uk>
2022-04-07 10:58:40 +0000

    Setup user authentication with django-allauth and customise its standard login, logout and signin templates

--- coderblog/settings.py
+++ coderblog/settings.py
@@ -40,12 +40,23 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'blog',
 ]
+
+# we only have 1 site
+SITE_ID = 1
+
+# redirecting back to homepage after login/out
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

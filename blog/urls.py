bfa801c071ca247d59a0b65828150cf70ea443c8
Charlie <ciharland@hotmail.co.uk>
2022-04-07 10:14:48 +0000

    Create and link post_detail template with slug url

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     # url (no extension) uses PostList class view and its the homepage
     path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>', views.PostDetail.as_view(), name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 # Create your views here.
@@ -11,3 +11,32 @@ class PostList(generic.ListView):
     template_name = 'index.html'
     # number of posts per page
     paginate_by = 6
+
+
+class PostDetail(View):
+    # the GET method to get the post from db
+    # standard arguments and keyword args
+    def get(self, request, slug, *args, **kwargs):
+        # getting all the published blogs
+        queryset = Post.objects.filter(status=1)
+        # getting the post with its slug
+        post = get_object_or_404(queryset, slug=slug)
+        # getting all the approved comments in date order
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        # boolean value for if the user liked this post or not
+        liked = False
+        # checking if the specific user liked it
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        # sending all this info to the render method
+        return render(
+            request,
+            "post_detail.html",  # required template
+            # dictionary with the context (data to be passed to template)
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+            }
+        )

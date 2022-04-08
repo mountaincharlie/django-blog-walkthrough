862f41fd2c0cafe7eacdcad0a33a06301bc2d6fe
Charlie <ciharland@hotmail.co.uk>
2022-04-08 11:31:53 +0000

    Add message functionality for user feedback

--- coderblog/settings.py
+++ coderblog/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -58,6 +59,15 @@ SITE_ID = 1
 # redirecting back to homepage after login/out
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# assigning tags different bootstrap classes so they appear different colors
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
